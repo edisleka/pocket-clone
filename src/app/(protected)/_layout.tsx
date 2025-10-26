585dff73bd7376ae2fa77d0f61b0c8a1e37d74a6
Edis Leka <edisleka@yahoo.gr>
2025-10-26 14:58:04 +0200

    Added user to sentry if authenticated

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -1,6 +1,22 @@
+import { useUser } from '@clerk/clerk-expo'
+import * as Sentry from '@sentry/react-native'
 import { Stack } from 'expo-router'
+import { useEffect } from 'react'
 
 export default function ProtectedLayout() {
+  const user = useUser()
+
+  useEffect(() => {
+    if (user) {
+      Sentry.setUser({
+        id: user.user?.id,
+        email: user.user?.emailAddresses[0].emailAddress,
+      })
+    } else {
+      Sentry.setUser(null)
+    }
+  }, [])
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name='(tabs)' />
